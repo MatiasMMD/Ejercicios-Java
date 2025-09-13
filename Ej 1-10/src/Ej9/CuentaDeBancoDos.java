fae6a0d6470deb2cc3b5a72a453a45b3667b7ae1
MatiasMMD <mmarinodans@gmail.com>
2025-09-13 12:30:07 -0300

    Agregue la mitad de los ejercicios del Ej9

---
+++ Ej 1-10/src/Ej9/CuentaDeBancoDos.java
@@ -0,0 +1,40 @@
+package Ej9;
+
+public class CuentaDeBancoDos {
+    // 3. Crea una clase BankAccount con el atributo privado balance. Implementa los métodos deposit(double amount) y withdraw(double amount) que validen las cantidades correctamente.
+    private double balance;
+
+    public CuentaDeBancoDos(double balance){
+        this.setBalance(balance);
+    }
+
+     public double getBalance(){
+            return this.balance;
+        }
+
+    public void setBalance(double balance){
+        if (balance > 0) {
+            this.balance = balance;
+        } else {
+            System.out.println("Balance inválido.");
+        }
+    }
+
+    public void deposito(double monto){
+        if (monto > 0) {
+            balance = balance + monto;
+            System.out.println("Su nuevo blance es de: $" + balance);
+        } else {
+            System.out.println("Ingrese un monto válido para depositar.");
+        }
+    }
+
+    public void retiro(double monto){
+        if (monto<balance || monto==balance) {
+            balance = balance - monto;
+            System.out.println("Realiza un retiro de: $" + monto + " y su nuevo balance es de: $" + balance);
+        } else {
+            System.out.println("Ingrese un monto válido para retirar");
+        }    
+    }
+}
---
+++ Ej 1-10/src/Ej9/Temperatura.java
@@ -0,0 +1,18 @@
+package Ej9;
+
+public class Temperatura {
+    // 5. Crea una clase Temperature con el atributo privado celsius. El método setCelsius(double celsius) solo debe aceptar valores entre -100 y 100.
+    private double celsius;
+
+    public void setCelsius(double celsius){
+        if (celsius >= -100 && celsius <= 100) {
+            this.celsius = celsius;            
+        } else {
+            System.out.println("Ingrese una temperatura válida");
+        }
+    }
+
+    public double getCelsius(){
+        return celsius;
+    }
+}
